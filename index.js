6b27c44eab6033cb7e007b7c7f2295e3a20ff67b
Berlandro <bryangodlive0@gmail.com>
2024-05-08 16:13:34 +0700

    feat: add cors at index

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ import mongoose from 'mongoose';
 import prodRouter from './routes/productsRoutes.js';
 
 const app = express();
-
 const PORT = process.env.PORT || 3000
+
+app.use(cors(
+    {
+        origin: ['http://localhost:5173'],
+        methods: ["POST", "GET", "PATCH", "DELETE"],
+        credentials: true
+    }
+))
+
 app.use(express.json())
-app.use(cors())
 app.use(prodRouter)
 
 app.listen(PORT, () => console.log(`Connected to port http://localhost:${PORT}`))
