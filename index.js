afebe7165535ba09adf332aa678b5dddc9dc4361
Berlandro <bryangodlive0@gmail.com>
2024-05-10 14:10:13 +0700

    feat: fix router bug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ['https://cemilin-api.vercel.app/', 'http://localhost:5173', 'https://cemilin-overview.vercel.app'],
+        origin: ['https://cemilin-api.vercel.app', 'http://localhost:5173', 'https://cemilin-overview.vercel.app'],
         methods: ["POST", "GET", "PATCH", "DELETE"],
         credentials: true
     }
